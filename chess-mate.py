c35bd8a9ce99d9b67784f407c79ba160b4665b34
Jakub <jakub.f@hotmail.co.uk>
2021-02-07 14:42:13 +0000

    Pawn movement and class rework

--- chess-mate.py
+++ chess-mate.py
@@ -1,145 +1,147 @@
-def board_reset(swap = False):
+def board_reset():
         """Create and set up board"""
-        if swap == False:
-            white = "12"
-            black = "78"
-        elif swap == True:
-            white = "78"
-            black = "12"
+        white = (1, 2)
+        black = (7, 8)
         board = {}
-        numbers = "87654321"
-        letters = "abcdefgh"
+        numbers = range(1,9)
         pieces = "rhbqkbhr"
-        for n in numbers:
-            for l in letters:
+        for y in numbers:
+            for x in numbers:
                 color = None
                 piece = None
                 square = None
-                if n in "81":
-                    piece = pieces[ord(l)-97]
-                # elif n in "1":
-                #     piece = pieces[-1-(ord(l)-97)]
-                elif n in "72":
+                if y in (1, 8):
+                    piece = pieces[y-1]
+                elif y in (2, 7):
                     piece = "p"
-                if n in white:
+                if y in white:
                     color = "w"
-                elif n in black:
+                elif y in black:
                     color = "b"
                 if color and piece:
                     square = {"color" : color, "piece" : piece}
-                board[l+n] = square
-        return (board, swap)
+                board[(x, y)] = square
+        return (board, {"turn": 0, "check": False, "en_passant": False, "history": []})
 
-def coord_calc(current, add_letter, add_number):
-    """Takes in a string of chess cordinates, position change by number of squares on each axis and returns calculated string"""
-    letter = chr(ord(current[0]) + add_letter)
-    number = str(int(current[1]) + add_number)
-    if letter not in "abcdefgh" or number not in "12345678":
-        return False
-    return letter + str(number)
+class Moves():
+    """Class for moves for each piece, each move returns a tuple of coordinates in format (move_to, attack_square, en_passant_eligible), 
+    en_assant_eligible returns coordinates of recent double pawn move or True if en passant move is valid to differentiate this attack"""
+    def __init__(self, board, info, start):
+        self.board = board
+        self.info = info
+        self.x = start[0]
+        self.y = start[1]
+        self.extent = range(1,9)
+        self.piece = self.board[start]
+        self.en_passant = self.info["en_passant"]
+        if self.piece["color"] == "w":
+            self.direction = 1
+        elif self.piece["color"] == "b":
+            self.direction = -1
+
+    def pawn_forward(self):
+        """Single move forward by pawn"""
+        move = (self.x, self.y + 1 * self.direction)
+        if self.board[move] or move[0] not in self.extent or move[1] not in self.extent:
+            move = False
+        return (move, None, False)
+
+    def pawn_double(self):
+        """Pawn double move"""
+        if (self.direction == 1 and self.y != 2) or (self.direction == -1 and self.y != 7):
+            print("test")
+            return (False, None, False)
+        move = (self.x, self.y + 2 * self.direction)
+        if self.board[move] and self.board[self.x, self.x + 1 * self.direction] or move[0] not in self.extent or move[1] not in self.extent:
+            move = False
+        return (move, None, move)
+    
+    def pawn_attack(self, x):
+        """diagonal pawn attack"""
+        move = (self.x + x, self.y + 1 * self.direction)
+        if self.board.get(move):
+            if self.board[move]["color"] != self.piece["color"]:
+                return (move, move, False)
+        return (False, None, False)
+    
+    def pawn_en_passant(self, x):
+        """en passant attack"""
+        attack = (self.x + x, self.y)
+        if self.en_passant == attack:
+            move = (self.x + 1, self.y + 1 * self.direction)
+            if not self.board[move] and not self.board[self.x, self.x + 1 * self.direction] or move[0] in self.extent or move[1] in self.extent:
+                return (move, attack, True)
+        return (False, None, True)
+
+class Pieces():
+    """Class for pieces control and moves"""
+    def __init__(self, board, info, start):
+        self.board = board
+        self.info = info
+        self.start = start
+        
+    def pawn_moves(self):
+        moves = Moves(self.board, self.info, self.start)
+        valid_moves = []
+        valid_moves.append(moves.pawn_forward())
+        valid_moves.append(moves.pawn_double())
+        valid_moves.append(moves.pawn_attack(1))
+        valid_moves.append(moves.pawn_attack(-1))
+        valid_moves.append(moves.pawn_en_passant(1))
+        valid_moves.append(moves.pawn_en_passant(-1))
+        valid_moves = tuple(filter(lambda move: move[0] != False, valid_moves))
+        return valid_moves
 
 class Game():
-    """Class responsible for movement, validation and anything to do with the game process"""
-    def __init__(self, board, move = None):
-        self.board = board[0]
-        self.swap = board[1]
-        self.move = move
+    """Class responsible for general game control"""
+    def __init__(self, game):
+        self.board = game[0]
+        self.info = game[1]
 
     def ascii(self):
         """Prints ASCII board in console"""
-        count = 0
-        set_up = ""
+        rows = ""
         rim = " +------------------------+ \n"
         letters = "   A  B  C  D  E  F  G  H  \n"
-        for square, value in self.board.items():
-            count += 1
-            placement = "."
-            if count == 1:
-                set_up = set_up + square[1] + "|"
-            if value:
-                placement = value["piece"]
-            set_up = set_up + " " + placement + " "
-            if count == 8:
-                set_up = set_up + "|" + square[1] + "\n"
-                count = 0
-        ascii_board = f"{letters}{rim}{set_up}{rim}{letters}"
-        return ascii_board
-    
-    def valid_moves(self, position):
-        """lists all valid moves for given location and piece present"""
-        piece = self.board[position]
-        moves = []
+        for y in range(1, 9):
+            row = ""
+            for x in range(1, 9):
+                if self.board[(x, y)]:
+                    piece = self.board[(x, y)].get("piece")
+                else:
+                    piece = "."
+                row = row + " " + piece + " "
+            rows = str(y) + "|" + row + "|" + str(y) + "\n" + rows
+        return print(f"{letters}{rim}{rows}{rim}{letters}")
+
+    def get_moves(self, start):
+        """Gets moves for specific piece"""
+        piece = self.board[start]
+        if not piece:
+            return False
+
+        pieces = Pieces(self.board, self.info, start)
+        pieces_dict = {"p": pieces.pawn_moves()}
+
+        return pieces_dict[piece["piece"]]
         
-        def pawn():
-            """Pawn check"""
-            if not self.swap:
-                bottom = "w"
-            elif self.swap:
-                bottom = "b"
-            direction = 1
-            if not piece["color"] == bottom:
-                direction = -1
-            square = coord_calc(position, 0, 1 * direction)
-            if not self.board[square]:
-                moves.append({"move": square})
-                if direction == 1:
-                    start = "2"
-                elif direction == -1:
-                    start = "7"
-                if start in position:
-                    square =  coord_calc(position, 0, 2 * direction)
-                    if not self.board[square]:
-                        moves.append({"move": square})
-            square = coord_calc(position, 1, 1 * direction)
-            if square:
-                if self.board[square]:
-                    if self.board[square].get("color") != piece["color"]:
-                        moves.append({"move": square, "attack": self.board[square]["piece"]})
-            square = coord_calc(position, -1, 1 * direction)
-            if square:
-                if self.board[square]:
-                    if self.board[square].get("color") != piece["color"]:
-                        moves.append({"move": square, "attack": self.board[square]["piece"]})
-            if direction == 1:
-                start = "5"
-            elif direction == -1:
-                start = "4"
-            if start in position:
-                special = coord_calc(position, -1, 0)
-                if self.board[square]:
-                    if self.board[special].get("special") == "pawn_special" and self.board[special]["color"] != piece["color"]:
-                        square = coord_calc(position, -1, 1 * direction)
-                        if not square:
-                            moves.append({"move": square, "attack": special})
-                            special = coord_calc(position, -1, 0)
-                special = coord_calc(position, -1, 0)
-                if self.board[square]:
-                    if self.board[special].get("special") == "pawn_special" and self.board[special]["color"] != piece["color"]:
-                        square = coord_calc(position, -1, 1 * direction)
-                        if not square:
-                            moves.append({"move": square, "attack": special})
-            return moves
-
-            def 
-
-        pieces = {"p": pawn()}
-        return pieces.get(piece["piece"], False)
-            
 
 
 
-    # def validate(self)
-    #     """validates the move"""
-    #     piece = 
-        
+
+
         
 
 
 board = board_reset()
-# print(board)
+
+print(board)
 round = Game(board)
-print(round.valid_moves("h7"))
 
-# print(round.ascii())
+# # print(round.valid_moves("h7"))
 
-# print(coord_calc("H3", -1, 1))
+round.ascii()
+
+print(round.get_moves((1, 2)))
+
+# # print(coord_calc("H3", -1, 1))
