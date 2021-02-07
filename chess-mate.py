eabf144febe2784e93ba0edb1c9ed9bb3174bd1f
Jakub <jakub.f@hotmail.co.uk>
2021-02-07 19:41:17 +0000

    Moves + testing

--- chess-mate.py
+++ chess-mate.py
@@ -82,60 +82,102 @@ class Moves():
             return (move, move, False)
         return (False, None, False)
 
-class Pieces():
-    """Class for pieces control and moves"""
-    def __init__(self, board, info, start):
-        self.board = board
-        self.info = info
-        self.start = start
-        self.moves = Moves(self.board, self.info, self.start)
-
     def loop_directions(self, directions):
+        """loops directional moves for bishop, rook and queen"""
         valid_moves = []
         for dir in directions:
             distance = 1
             while True:
-                move = self.moves.piece_move(distance * dir[0], distance * dir[1])
+                move = self.piece_move(distance * dir[0], distance * dir[1])
                 valid_moves.append(move)
                 if not self.board.get(move[1]):
                     distance = 1
                     break
                 distance += 1
-        valid_moves = tuple(filter(lambda move: move[0] != False, valid_moves))
+        valid_moves = tuple(filter(lambda move: move[0] != False, valid_moves))            
+
+class Pieces_Moves():
+    """Class for pieces control and moves"""
+    def __init__(self, board, info):
+        self.board = board
+        self.info = info
+ 
         
-    def pawn_moves(self):
+    def pawn_moves(self, start):
         """returns a tuple of valid moves for pawn"""
+        moves = Moves(self.board, self.info, start)
         valid_moves = []
-        valid_moves.append(self.moves.pawn_forward())
-        valid_moves.append(self.moves.pawn_double())
-        valid_moves.append(self.moves.pawn_attack(1))
-        valid_moves.append(self.moves.pawn_attack(-1))
-        valid_moves.append(self.moves.pawn_en_passant(1))
-        valid_moves.append(self.moves.pawn_en_passant(-1))
+        valid_moves.append(moves.pawn_forward())
+        valid_moves.append(moves.pawn_double())
+        valid_moves.append(moves.pawn_attack(1))
+        valid_moves.append(moves.pawn_attack(-1))
+        valid_moves.append(moves.pawn_en_passant(1))
+        valid_moves.append(moves.pawn_en_passant(-1))
         return tuple(filter(lambda move: move[0] != False, valid_moves))
     
-    def rook_moves(self):
-        """return tuple of valid moves for Rook"""
+    def rook_moves(self, start):
+        """returns tuple of valid moves for Rook"""
+        moves = Moves(self.board, self.info, start)
         directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        return self.loop_directions(directions)
+        return moves.loop_directions(directions)
     
-    def horse_moves(self):
-        """return tuple of valid moves for Horse (Knight)"""
+    def horse_moves(self, start):
+        moves = Moves(self.board, self.info, start)
+        """returns tuple of valid moves for Horse (Knight)"""
         valid_moves = []
         distances = ((2, 3), (2, -3), (-2, 3), (-2, -3), (3, 2), (3, -2), (-3, 2), (-3, -2))
         for dist in distances:
-            valid_moves.append(self.moves.piece_move(dist[0], dist[1]))
+            valid_moves.append(moves.piece_move(dist[0], dist[1]))
         return tuple(filter(lambda move: move[0] != False, valid_moves))           
 
-    def bishop_moves(self):
-        """return tuple of valid moves for Bishop"""
+    def bishop_moves(self, start):
+        moves = Moves(self.board, self.info, start)
+        """returns tuple of valid moves for Bishop"""
         directions = ((1, 1), (1, -1), (-1, 1), (-1, -1))
-        return self.loop_directions(directions)
+        return moves.loop_directions(directions)
 
-    def queen_moves(self):
-        """return tuple of valid moves for Queen"""
+    def queen_moves(self, start):
+        moves = Moves(self.board, self.info, start)
+        """returns tuple of valid moves for Queen"""
         directions = ((1, 1), (1, -1), (-1, 1), (-1, -1), (1, 0), (-1, 0), (0, 1), (0, -1))
-        return self.loop_directions(directions)
+        return moves.loop_directions(directions)
+
+    def king_moves(self, start):
+        moves = Moves(self.board, self.info, start)
+        """returns tuple of valid moves for King"""
+        color = self.board[start]["color"]
+        valid_moves = []
+        distances = ((1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (2, -1), (-2, 1), (-2, -1))
+        for dist in distances:
+            valid_moves.append(moves.piece_move(dist[0], dist[1]))
+        valid_moves = filter(lambda move: move[0] != False, valid_moves)
+        return self.king_check(moves, color)
+    
+    def king_check(self, moves, color):
+        """checks for all possible attack moves on the board and filters the King moves down to exlude all attack moves, or elsy anythin that would cause a check condition"""
+        valid_moves =[]
+        for start, piece in self.board.items():
+            print(piece["color"] != color)
+            if piece["color"] != color:
+                attack_moves = tuple(map(lambda attack_move: attack_move[1], filter(lambda move: move[1] != None and move[2] != True, self.get_moves(start))))
+                valid_moves = valid_moves + filter(lambda move: move[0] not in attack_moves, moves)
+        return tuple(valid_moves)
+
+    def get_moves(self, start):
+        """Checks for the piece at start location and calls in move function accordingly to get valid pieces"""
+        piece = self.board[start]
+        if not piece:
+            return False
+
+        pieces_dict = {
+            "p": self.pawn_moves,
+            "r": self.rook_moves,
+            "h": self.horse_moves,
+            "b": self.bishop_moves,
+            "q": self.queen_moves,
+            "k": self.king_moves            
+            }
+        return pieces_dict[piece["piece"]](start)        
 
 class Game():
     """Class responsible for general game control"""
@@ -160,24 +202,46 @@ class Game():
         return print(f"{letters}{rim}{rows}{rim}{letters}")
 
     def get_moves(self, start):
-        """Gets moves for specific piece"""
-        piece = self.board[start]
-        if not piece:
-            return False
+        """Gets moves for specific piece via function in Pieces class"""
+        pieces = Pieces_Moves(self.board, self.info)
+        return pieces.get_moves(start)
 
-        pieces = Pieces(self.board, self.info, start)
-        pieces_dict = {
-            "p": pieces.pawn_moves(),
-            "r": pieces.rook_moves(),
-            "h": pieces.horse_moves(),
-            "b": pieces.bishop_moves(),
-            "q": pieces.queen_moves(),
-            # "k": pieces.king_moves()            
-            }
-
-        return pieces_dict[piece["piece"]]
+    def move_piece(self, start, end):
+        for move in self.get_moves(start):
+            if move[0] == end:
+                self.board[end] = self.board[start]
+                self.board[start] = None
+                self.board[move[1]] = None
+                # print("valid move")
+                return (
+                self.board,
+                    {
+                        "turn": self.info["turn"] + 1,
+                        "check": False,
+                        "en_passant": move[2],
+                        "history": self.info["history"] + [f"{start[0]}{start[1]}{end[0]}{end[1]}"]
+                    }
+                )
+        # print("invalid move")
+        return False
         
-
+    def play(self):
+        while True:
+            start = input("Move from:")
+            start = (ord(start[0])-96, int(start[1]))
+            # print(start)
+            end = input("Move to:")
+            end = (ord(end[0])-96, int(end[1]))
+            # print(end)
+            update_game = self.move_piece(start, end)
+            if update_game:
+                self.board = update_game[0]
+                self.info = update_game[1]
+            else:
+                print("Invalid move, try again.")
+            self.ascii()
+            # for key, value in self.board.items():
+            #     print(f"{key} {value}")
 
 
 
@@ -186,14 +250,15 @@ class Game():
 
 
 board = board_reset()
-
 print(board)
-round = Game(board)
+new_game = Game(board)
+new_game.ascii()
+new_game.play()
 
 # # print(round.valid_moves("h7"))
 
-round.ascii()
+# round.ascii()
 
-print(round.get_moves((2, 1)))
+# print(round.get_moves((2, 1)))
 
 # # print(coord_calc("H3", -1, 1))
